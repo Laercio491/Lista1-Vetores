59bae1719e2139c91e09e0b4221e34ff8226d9de
Laercio491 <laercio.antonio@alunos.ifsuldeminas.edu.br>
2025-09-05 23:15:49 -0300

    confirmação

--- Lista1/Exercicio3.cs
+++ Lista1/Exercicio3.cs
@@ -1,4 +1,3 @@
-﻿
 ﻿using System;
 using MinhaBiblioteca;
 
@@ -17,24 +16,4 @@ class Exercicio3
         Console.WriteLine("\nO menor valor do vetor: " + t);
         Console.ReadKey();
     }
-=======
-﻿using System;
-using MinhaBiblioteca;
-
-class Exercicio3
-
-{
-    static void Main()
-    {
-        int n, t;
-        Console.WriteLine("Tamanho do vetor: ");
-        n = int.Parse(Console.ReadLine());
-        int[] meuVetor = new int[n];
-        Biblioteca.gerarVetor(meuVetor);
-        Biblioteca.mostrarVetor(meuVetor);
-        t = Biblioteca.menorVetor(meuVetor);
-        Console.WriteLine("\nO menor valor do vetor: " + t);
-        Console.ReadKey();
-    }
->>>>>>> a579633383ade4ce20b458300cbc7fde09e8d099
 }
--- Lista1/obj/Debug/net9.0/Lista1.AssemblyInfo.cs
+++ Lista1/obj/Debug/net9.0/Lista1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bdc255fd0d51e243a302cf742d0b21d896cb8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27eb3c92a1313aeff5f28d17681f1b6696575ee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
